4cf98613f8b7372ffea01d4317f4f7db3564b67e
Revitte <saviocqr@gmail.com>
2025-08-01 12:15:38 -0300

    <docs>(Config Inicial): Organizando pacotes,  documentos configurações iniciais e dependências

---
+++ src/main/java/com/OndeVamos/OndeVamosApplication.java
@@ -0,0 +1,13 @@
+package com.OndeVamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndeVamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndeVamosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OndeVamos/OndeVamos/OndeVamosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OndeVamos.OndeVamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndeVamosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
